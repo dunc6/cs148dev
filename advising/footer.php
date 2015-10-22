ff7d471aa7fa765c18d4647dd99a1f6df305c563
Tyler Duncan Whitaker <twhitake@silk2.uvm.edu>
2015-10-22 19:00:16 -0400

    .gitignore is now working

---
+++ advising/footer.php
@@ -0,0 +1,8 @@
+<!-- @@@@@@@@@@@@@@@@@@@@@@     Footer            @@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+<footer>
+    <p>Web site designed by Duncan Whitaker</p>
+</footer>
+<!-- @@@@@@@@@@@@@@@@@@@@      Ends Footer        @@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+
+</body>
+</html>
